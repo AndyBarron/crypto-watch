729768a91bda1e79ab55069c183676c5e5cfab44
Andy Barron <AndrewLBarron@gmail.com>
2018-01-18 23:44:45 -0800

    smarter rounding, smart attachment merging

--- app/attachments.mjs
+++ app/attachments.mjs
@@ -2,28 +2,33 @@ import moment from 'moment';
 import numeral from 'numeral';
 import { getSummary, getInfo } from './api.mjs';
 
-const SUPPLY_FORMAT = '0,0.0a';
-const VALUE_FORMAT = '$0,0.00';
 const MARKET_CAP_FORMAT = '$0,0.0a';
 const PERCENT_FORMAT = '+0,0.0';
+const SUPPLY_FORMAT = '0,0.0a';
+const VALUE_ROUNDED_FORMAT = '$0,0.00';
 
 // eslint-disable-next-line import/prefer-default-export
 export const createAttachmentsForIdentifier = async (identifier) => {
   const info = await getInfo(identifier);
   if (!info) {
-    return [{ text: `_No such cryptocurrency: "${identifier}"_` }];
+    return [{
+      mrkdwn_in: ['text'],
+      text: `_No such cryptocurrency: "${identifier}"_`,
+    }];
   }
   const {
     fullName, imageUrl, infoUrl, symbol,
   } = info;
   const summary = await getSummary(symbol);
+  const value = summary.price;
+  const valueText = value <= 10 ? `${value}` : numeral(value).format(VALUE_ROUNDED_FORMAT);
   const details = {
     title: fullName,
     title_link: infoUrl,
     thumb_url: imageUrl,
     mrkdwn_in: ['text'],
     text: [
-      `*Value: ${numeral(summary.price).format(VALUE_FORMAT)}*`,
+      `*Value: ${valueText}*`,
       `_Market cap: ${numeral(summary.marketCap).format(MARKET_CAP_FORMAT)}_`,
       `_Supply: ${numeral(summary.supply).format(SUPPLY_FORMAT)}_`,
     ].join('\n'),
@@ -41,6 +46,14 @@ export const createAttachmentsForIdentifier = async (identifier) => {
       text: `*${percent}%* since ${moment(timestamp).fromNow()} _<!date^${moment(timestamp).unix()}^({date_short})| >_`,
       mrkdwn_in: ['text'],
     });
+    for (let i = 0; i < attachments.length - 1; i++) {
+      const [current, next] = attachments.slice(i);
+      if (current.color === next.color) {
+        current.text += `\n${next.text}`;
+        attachments.splice(i + 1, 1);
+        i--;
+      }
+    }
   }
   return attachments;
 };
